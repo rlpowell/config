99ee11d4d17744f1215c98279c7f410e738f4f9f
Robin Lee Powell <rlpowell@digitalkingdom.org>
2012-11-04 17:15:21 -0800

    Moved everything to make room for bin dir.

--- install.sh
+++ install.sh
@@ -3,12 +3,15 @@ then
   echo "Need a \$HOME."
   exit 1
 fi
+
+echo "Setting up dot files."
+
 for file in gitconfig vimrc tmux.conf muttrc muttrc-gmail \
                         inputrc bash_profile bashrc zshenv \
                         zshrc
 do
   dotfile="$HOME/.$file"
-  gitfile="$(pwd)/$file"
+  gitfile="$(pwd)/dotfiles/$file"
 
   if [ -e "$dotfile" -a ! -h "$dotfile" ]
   then
@@ -20,7 +23,7 @@ do
   ln -sf "$gitfile" "$dotfile"
 done
 
-ln -sfT "$(pwd)/vim" ~/.vim
+ln -sfT "$(pwd)/dotfiles/vim" ~/.vim
 # test -d ~/.vim/ || ln -sfT "$(pwd)/vim/" ~/.vim
 
 # treed has this submoduled to https://github.com/joelthelion/autojump
