38794d147430a32566cc314392e24854c6d5a7d9
Robin Lee Powell <rlpowell@digitalkingdom.org>
2015-04-15 17:11:49 -0700

    Better paths.

--- dotfiles/fzf.zsh
+++ dotfiles/fzf.zsh
@@ -1,20 +1,20 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" =~ "/home/rlpowell/src/fzf/bin" ]]; then
-  export PATH="$PATH:/home/rlpowell/src/fzf/bin"
+if [[ ! "$PATH" =~ "/home/rlpowell/.fzf/bin" ]]; then
+  export PATH="$PATH:/home/rlpowell/.fzf/bin"
 fi
 
 # Man path
 # --------
-if [[ ! "$MANPATH" =~ "/home/rlpowell/src/fzf/man" && -d "/home/rlpowell/src/fzf/man" ]]; then
-  export MANPATH="$MANPATH:/home/rlpowell/src/fzf/man"
+if [[ ! "$MANPATH" =~ "/home/rlpowell/.fzf/man" && -d "/home/rlpowell/.fzf/man" ]]; then
+  export MANPATH="$MANPATH:/home/rlpowell/.fzf/man"
 fi
 
 # Auto-completion
 # ---------------
-# [[ $- =~ i ]] && source "/home/rlpowell/src/fzf/shell/completion.zsh"
+# [[ $- =~ i ]] && source "/home/rlpowell/.fzf/shell/completion.zsh"
 
 # Key bindings
 # ------------
-source "/home/rlpowell/src/fzf/shell/key-bindings.zsh"
+source "/home/rlpowell/.fzf/shell/key-bindings.zsh"
 
