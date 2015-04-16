482a3c142a2111fed8a4aae6b9a79032844bdb5d
Robin Lee Powell <rlpowell@digitalkingdom.org>
2015-04-15 17:30:53 -0700

    Moved the bindings around.

--- upgrade.sh
+++ upgrade.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 
-cd /home/rlpowell/config
+if [ ! -d "dotfiles" ]
+then
+  echo "Run this from the config/ directory, please ; there should be a 'dotfiles' dir in the place you run this."
+  exit 1
+fi
+
+if [ ! "$HOME" ]
+then
+  echo "Need a \$HOME."
+  exit 1
+fi
+
+if [ -h "/home/rlpowell/.fzf.bash" ]
+then
+  rm "/home/rlpowell/.fzf.bash"
+fi
+if [ -h "/home/rlpowell/.fzf.zsh" ]
+then
+  rm "/home/rlpowell/.fzf.zsh"
+fi
+
 git pull
 yes | vim -c ':PlugInstall' -c ':qa'
 yes | vim -c ':PlugUpdate' -c ':qa'
+yes | ~/.fzf/install
